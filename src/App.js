6757e820f954d125abf7b058a31f87ae7ef98085
Sachin Parmar <parmarsachin707@gmail.com>
2022-11-08 20:49:45 +0530

    Created Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 
 function App() {
   return (
     <>
-      <h1 className='text-5xl bg-red-400' >Hello World!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = { <Home /> } />
+          <Route path = "/sign-in" element = { <SignIn /> } />
+          <Route path = "/sign-up" element = { <SignUp /> } />
+          <Route path = "/forgot-password" element = { <ForgotPassword /> } />
+          <Route path = "/offers" element = { <Offers /> } />
+          <Route path = "/profile" element = { <Profile /> } />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
