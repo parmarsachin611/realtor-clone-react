4453960d4d4c496597b14272316b25ddbfb73d9a
Sachin Parmar <parmarsachin707@gmail.com>
2022-11-20 15:35:54 +0530

    Creatde Spinner

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router';
 import {OAuthStatus} from '../hooks/OAuthStatus';
+import Spinner from './Spinner';
 
 export default function PrivateRoute() {
     const { loggedIn, checkingStatus } = OAuthStatus()
     if (checkingStatus) {
-        return <h3>Loading...</h3>
+        return <h3> <Spinner /> </h3>
     }
     return loggedIn ? <Outlet /> : <Navigate to={"/sign-in"} />
 }
