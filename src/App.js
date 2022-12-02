f2d1651e1bc9dc24c52f55ea636be53d79d74018
Sachin Parmar <parmarsachin707@gmail.com>
2022-12-02 19:53:45 +0530

    Edit Listing Page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import SignUp from "./pages/SignUp";
 import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
 
 function App() {
   return (
@@ -22,11 +23,14 @@ function App() {
           <Route path = "/sign-up" element = { <SignUp /> } />
           <Route path = "/forgot-password" element = { <ForgotPassword /> } />
           <Route path = "/offers" element = { <Offers /> } />
+          <Route path="/profile" element = { <PrivateRoute /> } >
+            <Route path = "/profile" element = { <Profile /> } />
+          </Route>
           <Route path="/create-listing" element = { <PrivateRoute /> } >
             <Route path = "/create-listing" element = { <CreateListing /> } />
           </Route>
-          <Route path="/profile" element = { <PrivateRoute /> } >
-            <Route path = "/profile" element = { <Profile /> } />
+          <Route path="/edit-listing" element = { <PrivateRoute /> } >
+            <Route path = "/edit-listing/:listingId" element = { <EditListing /> } />
           </Route>
         </Routes>
       </BrowserRouter>
